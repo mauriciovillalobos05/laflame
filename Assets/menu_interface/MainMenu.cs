f8e25d5182caf97d6c3f09ee700ded8e902903b3
Upland1 <107065818+Upland1@users.noreply.github.com>
2025-05-25 21:34:06 -0600

    Add Main Menu

---
+++ Assets/menu_interface/MainMenu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
